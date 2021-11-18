3792a9be14ad3f57abaf70344f5adfdc4e332feb
Jordan <isakaru12@yahoo.com>
2021-11-18 12:17:52 -0500

    Laptop controls and framework for additional control

---
+++ Assets/HelicopterHead.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterHead : MonoBehaviour
+{
+    public PlayerInput playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Asks which device is being used (maybe this shouldn't be handled in the Helicopter Head class)
+        // if android, playerInput = new AndroidPlayerInput();
+        // maybe in the awake method, we define an enum which can decide which device you're on
+        if (true)
+        {
+            playerInput = new WindowsPlayerInput();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInput.InputHandling(this.gameObject);
+    }
+}
