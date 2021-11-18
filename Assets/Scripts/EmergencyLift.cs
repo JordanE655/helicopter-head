23497648eacae9973516d1b01bf64ae707cfd49e
Jordan <isakaru12@yahoo.com>
2021-11-18 11:11:18 -0500

    Updated project version

--- Assets/Scripts/EmergencyLift.cs
+++ Assets/Scripts/EmergencyLift.cs
@@ -17,37 +17,12 @@ public class EmergencyLift : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, Vector3.down * distance, Color.red, .001f);
-        if (Physics.Raycast(transform.position, Vector3.down, distance))
-        {
-            if (!deployed)
-            {
-                InvertBroccoli();
-            }
-        }
-        else if (Physics.Raycast(transform.position, Vector3.up, distance))
-        {
-            if (deployed)
-            {
-                RevertBroccoli();
-            }
-        }
 
     }
 
-    // Inverts the gravity
-    void InvertBroccoli()
+    // Fixed Update is called at a consistent rate
+    private void FixedUpdate()
     {
-        Debug.Log("Gravity has been INverted");
-        deployed = true;
-        Physics.gravity = new Vector3(0, 3, 0);
-    }
-
-    // Reverts the gravity
-    void RevertBroccoli()
-    {
-        Debug.Log("Gravity has been reverted");
-        deployed = false;
-        Physics.gravity = new Vector3(0, -6, 0);
+        
     }
 }
